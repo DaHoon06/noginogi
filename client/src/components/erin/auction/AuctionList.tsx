d77a97d664b1149285c15dc2f5a4e72ff4d6f51f
dahoon06 <dahoon226@naver.com>
2024-12-03 17:03:52 +0900

    feat: 모달 추가
    todo 리렌더링 제어 필요

--- client/src/components/erin/auction/AuctionList.tsx
+++ client/src/components/erin/auction/AuctionList.tsx
@@ -8,48 +8,76 @@ import {
   Tr,
   Th,
   Td,
-  TableContainer, } from "@chakra-ui/react";
+  TableContainer,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton, Button,
+} from "@chakra-ui/react";
 import {useAuctionListQuery} from "@state/queries/hooks";
 
 export const AuctionList = (): ReactElement | null => {
-
   const {data, isLoading} = useAuctionListQuery({auction_item_category: '검'})
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   if (isLoading) return null;
 
   return (
-    <TableContainer>
-      <Table variant={'simple'}>
-        <Thead>
-          <Tr>
-            <Th>이미지</Th>
-            <Th>이름</Th>
-            <Th>수량</Th>
-            <Th>가격</Th>
-            <Th>만료일</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data && data.auction_item.map((item, index) => (
-            <Tr key={`${item.item_name}_${index}`}>
-              <Td>No Image</Td>
-              <Td>{item.item_display_name}</Td>
-              <Td>{item.item_count}</Td>
-              <Td>{item.auction_price_per_unit.toLocaleString()}원</Td>
-              <Td>{new Date(item.date_auction_expire).toLocaleDateString()}</Td>
-              <Td>
-                {item.item_option.map((opt, optIndex) => (
-                  <div key={`${item.item_name}_option_${optIndex}`}>
-                    {opt.option_desc}: {opt.option_value}
-                  </div>
-                ))}
-              </Td>
+    <>
+      <TableContainer>
+        <Table variant={'simple'}>
+          <Thead>
+            <Tr>
+              <Th>이미지</Th>
+              <Th>이름</Th>
+              <Th>수량</Th>
+              <Th>가격</Th>
+              <Th>만료일</Th>
             </Tr>
-          ))}
-          {data && data.auction_item.length === 0 && (<Tr><Td colSpan={6} textAlign="center" verticalAlign="middle">No Data.</Td></Tr>)}
-        </Tbody>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {data && data.auction_item.map((item, index) => (
+              <Tr key={`${item.item_name}_${index}`} onClick={onOpen}>
+                <Td>No Image</Td>
+                <Td>{item.item_display_name}</Td>
+                <Td>{item.item_count}</Td>
+                <Td>{item.auction_price_per_unit.toLocaleString()}원</Td>
+                <Td>{new Date(item.date_auction_expire).toLocaleDateString()}</Td>
+                <Td>
+                  {item.item_option.map((opt, optIndex) => (
+                    <div key={`${item.item_name}_option_${optIndex}`}>
+                      {opt.option_desc}: {opt.option_value}
+                    </div>
+                  ))}
+                </Td>
+              </Tr>
+            ))}
+            {data && data.auction_item.length === 0 && (<Tr><Td colSpan={6} textAlign="center" verticalAlign="middle">No Data.</Td></Tr>)}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            롸
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
