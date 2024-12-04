53e8ca1eae30632d50f8a1710c944a8ac3e2281e
dahoon06 <dahoon226@naver.com>
2024-12-04 16:57:40 +0900

    fix: 카테고리 항목 수정

--- application/src/components/erin/auction/AuctionCategory.tsx
+++ application/src/components/erin/auction/AuctionCategory.tsx
@@ -1,36 +1,30 @@
-'use client';
+"use client";
 
-import {Fragment, ReactElement, useState} from "react";
-import {data} from '@state/initialize/auctionCategories.json';
-import * as S from './AuctionCategory.styled';
+import { Fragment, ReactElement, useState } from "react";
+import { data } from "@state/initialize/auctionCategories.json";
+import * as S from "./AuctionCategory.styled";
 
 type SelectedAuctionCategoryType = {
   cat1: string;
   cat2: string;
-  cat3: string;
-}
-
+};
 
 export const AuctionCategory = (): ReactElement => {
   const [auctionCategory] = useState(data);
-  const [currentCategory, setCurrentCategory] = useState<SelectedAuctionCategoryType>({
-    cat1: '',
-    cat2: '',
-    cat3: '',
-  });
+  const [currentCategory, setCurrentCategory] =
+    useState<SelectedAuctionCategoryType>({
+      cat1: "",
+      cat2: "",
+    });
 
   const handleCategoryClick = (label: string, step: number) => {
     setCurrentCategory((prev) => {
       const updatedCategory = { ...prev };
       if (step === 1) {
         updatedCategory.cat1 = label;
-        updatedCategory.cat2 = '';
-        updatedCategory.cat3 = '';
+        updatedCategory.cat2 = "";
       } else if (step === 2) {
         updatedCategory.cat2 = label;
-        updatedCategory.cat3 = '';
-      } else {
-        updatedCategory.cat3 = label;
       }
       return updatedCategory;
     });
@@ -38,10 +32,7 @@ export const AuctionCategory = (): ReactElement => {
 
   return (
     <S.Container>
-      <S.Title>카테고리 선택</S.Title>
-      1. {currentCategory.cat1}
-      2. {currentCategory.cat2}
-      3. {currentCategory.cat3}
+      <S.Title>카테고리</S.Title>
       {auctionCategory.map((category, categoryIndex) => {
         const { label, children } = category;
         return (
@@ -55,20 +46,13 @@ export const AuctionCategory = (): ReactElement => {
             {currentCategory.cat1 === label && (
               <S.ChildrenContainer>
                 {children.map((item, itemIndex) => (
-                  <S.ChildCategory key={`${label}_${item.category}_${itemIndex}`}>
+                  <S.ChildCategory key={`${label}_${item.label}_${itemIndex}`}>
                     <S.Category
-                      isSelected={currentCategory?.cat2 === item.category}
-                      onClick={() => handleCategoryClick(item.category, 2)}
+                      isSelected={currentCategory?.cat2 === item.label}
+                      onClick={() => handleCategoryClick(item.label, 2)}
                     >
-                      {item.category}
+                      {item.label}
                     </S.Category>
-                    {currentCategory.cat2 === item.category && (
-                      <>
-                        {item.items.map((v) => (
-                          <S.Item key={`${item.category}_${v.label}`} onClick={() => handleCategoryClick(v.value, 3)}>{v.label}</S.Item>
-                        ))}
-                      </>
-                    )}
                   </S.ChildCategory>
                 ))}
               </S.ChildrenContainer>
@@ -78,4 +62,4 @@ export const AuctionCategory = (): ReactElement => {
       })}
     </S.Container>
   );
-}
+};
